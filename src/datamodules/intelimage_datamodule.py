2243b4b82c712cc2a7da3eb27121b2bbdd11e0bd
Anant Gupta <anantgupta129@gmail.com>
2022-12-30 10:47:00 +0530

    added dataloaders

---
+++ src/datamodules/intelimage_datamodule.py
@@ -0,0 +1,103 @@
+from pathlib import Path
+from typing import Any, Dict, Optional, Tuple
+
+import pytorch_lightning as pl
+import torchvision.transforms as T
+from torch.utils.data import DataLoader, Dataset
+from torchvision.datasets import ImageFolder
+
+
+class IntelImgClfDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        data_dir: str = "data/",
+        batch_size: int = 256,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+    ):
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        self.data_dir = Path(data_dir)
+
+        # data transformations
+        self.transforms = T.Compose(
+            [
+                T.ToTensor(),
+                T.Resize((224, 224)),
+                T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            ]
+        )
+
+        self.data_train: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+
+    @property
+    def num_classes(self):
+        return len(self.data_train.classes)
+
+    @property
+    def classes(self):
+        return self.data_train.classes
+
+    def prepare_data(self):
+        """Download data if needed.
+        Do not use it to assign state (self.x = y).
+        """
+        pass
+
+    def setup(self, stage: Optional[str] = None):
+        """Load data. Set variables: `self.data_train`, `self.data_val`, `self.data_test`.
+        
+        This method is called by lightning with both `trainer.fit()` and `trainer.test()`, so be
+        careful not to execute things like random split twice!
+        """
+        # load and split datasets only if not loaded already
+        if not self.data_train and not self.data_test:
+            trainset = ImageFolder(self.data_dir / "train", transform=self.transforms)
+            testset = ImageFolder(self.data_dir / "test", transform=self.transforms)
+            valset = ImageFolder(self.data_dir / "val", transform=self.transforms)
+
+            self.data_train, self.data_test, self.data_val = trainset, testset, valset
+
+    def train_dataloader(self):
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def teardown(self, stage: Optional[str] = None):
+        """Clean up after fit or test."""
+        pass
+
+    def state_dict(self):
+        """Extra things to save to checkpoint."""
+        return {}
+
+    def load_state_dict(self, state_dict: Dict[str, Any]):
+        """Things to do when loading checkpoint."""
+        pass
