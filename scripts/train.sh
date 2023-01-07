a46f0886858f83d040d64aa502ec0ce704844c51
Anant Gupta <anantgupta129@gmail.com>
2023-01-07 12:44:03 +0000

    training with hyper param

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,4 @@
-python train.py datamodule.num_workers=8
+python train.py datamodule.num_workers=8 \
+    model.net.model_name=efficientnet_b3a model.optimizer._target_=torch.optim.Adam \
+    model.optimizer.lr=0.0052086233463775385 model.optimizer.weight_decay=0 \
+    datamodule.batch_size=32 trainer.max_epochs=12
