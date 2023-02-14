34595ab6393e86f7994431e25d7bceb6a59155b4
Anant Gupta <anantgupta129@gmail.com>
2023-02-14 13:57:57 +0530

    hyper parameter tuning with optuna

--- hyperparameter_tuning/src/utils/metric.py
+++ hyperparameter_tuning/src/utils/metric.py
@@ -11,9 +11,7 @@ from pytorch_lightning import LightningDataModule, LightningModule
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def plot_confusion_matrix(
-    cm, classes, title="Confusion matrix", cmap=plt.cm.Blues
-):
+def plot_confusion_matrix(cm, classes, title="Confusion matrix", cmap=plt.cm.Blues):
     """
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
@@ -25,7 +23,7 @@ def plot_confusion_matrix(
     tick_marks = np.arange(len(classes))
     plt.xticks(tick_marks, classes, rotation=45)
     plt.yticks(tick_marks, classes)
-    
+
     thresh = cm.max() / 2.0
 
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
